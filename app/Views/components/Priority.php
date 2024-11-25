7f25a6cfa7100d05e4e387a19ec7f8a02a2e5a81
Matthias Bernouy <matt.bernouy@orange.fr>
2024-11-25 16:06:18 +0100

    3 composants + css Matthias

---
+++ app/Views/components/Priority.php
@@ -0,0 +1,19 @@
+<?php 
+
+function Priority(int $nbActive){
+
+	$str = '<div class="shape__list">';
+
+	for ($i=0; $i < $nbActive; $i++) { 
+		$str .= '<div class="shape shape__active"></div>';
+	}
+
+	for ($i=0; $i < 4 - $nbActive; $i++) { 
+		$str .= '<div class="shape"></div>';
+	}
+
+	$str .= '</div>';
+
+	return $str;
+	
+}
