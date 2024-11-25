de8c6e44330c8a040ddd9825e794cd0691e64f18
LovisCoding <arthur.lecomte@etu.univ-lehavre.fr>
2024-11-25 11:26:42 +0100

    Add initial views, controllers, and layout for home and profile pages / creation of the navbar

--- app/Controllers/Profil.php
+++ app/Controllers/Profil.php
@@ -2,13 +2,13 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
+class Profil extends BaseController
 {
     public function index() : void
     {
         echo view('layout/header');
         echo view('layout/navbar');
-        echo view('pages/home'); 
+        echo view('pages/profil'); 
         echo view('layout/footer') ;
     }
 }
---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="<?= $_SERVER['REQUEST_URI'] == '/' ? '#' : '/'?>" style="margin-left: 20px;">
+			<img src="assets/imgs/logo.svg" width="150" height="80" alt="">
+		</a>
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item">
+				<a class="nav-link d-flex flex-column align-items-center <?= $_SERVER['REQUEST_URI'] == '/profil' ? 'nav-selected' : ''?>" href="/profil">
+					<img src="/assets/imgs/profil.svg" width="30" height="30" class="d-inline-block align-top" alt="Profil">
+					<span class="">Profil</span>
+				</a>
+			</li>
+		</ul>
+</nav>
