c5faff6e4538cd08c54359b2dffbbcc241651f36
LovisCoding <arthur.lecomte@etu.univ-lehavre.fr>
2024-11-26 10:57:43 +0100

    Profil page

--- app/Views/components/button.php
+++ app/Views/components/button.php
@@ -1,14 +1,15 @@
 <?php
-function button(string $svgName = null, string $text, string $type= 'basique') : string {
+function button(string $svgName = null, string $text, string $id , string $type= 'basique' ) : string {
 	$color = 'btn-grey';
 	if ($type == 'danger') $color= 'btn-danger';
+	if ($type == 'marron') $color= 'btn-marron';
 	
 	$str = "<div class=\"button\">
-	<button class=\"btn d-flex align-items-center {$color}\">";
+	<button class=\"btn d-flex align-items-center w-100 justify-content-center {$color}\" id=\"{$id}\">";
 
 	if ($svgName) 
 		$str .= '<img class="mx-2"  src="assets/imgs/' . $svgName . '.svg" alt="' . $svgName . '" width="20px" height="20px">';
 	
-	$str .= $text . '</button>';
+	$str .= $text . '</button> </div>';
 	return $str;
 }
--- app/Views/pages/home/newTache.php
+++ app/Views/pages/home/newTache.php
@@ -1,5 +1,5 @@
 <?php
 include __DIR__ . '/../../components/button.php';
 function newTache() {
-	return button('plus', 'Nouvelle tâche');
+	return button('plus', 'Nouvelle tâche', 'btn-new-tache','basique', );
 }
