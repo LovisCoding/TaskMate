a57cdf4e34cb454fffd5998941ab2834ca42408a
KLS <kyliannlvs2@gmail.com>
2024-11-26 11:14:39 +0100

    Finalisation page Home

--- app/Views/pages/home/home.php
+++ app/Views/pages/home/home.php
@@ -6,44 +6,73 @@ include 'card.php';
 include __DIR__.'/../../components/CalendarRange.php';
 include __DIR__.'/../../components/CalendarItemTitle.php';
 include 'newTache.php';
-?>
-<div class="pt-4"></div>
-<div class="d-flex justify-content-between mx-4" id="vues">
-	<div class="left d-flex">
-		<?= tabs() ?>
-		<?= export() ?>
-		<?= filter() ?>
-	</div>
-	<div class="right">
-	<?= newTache() ?>	
-	</div>	
-	
-</div>
-<div class="d-flex justify-content-center mt-4">
-	<?=CalendarRange(new DateTime(), 5)?>
-</div>
-<?php
-$_GET['date'] = '2021-01-01';
-$_GET['nb'] = 7;
 
-$nb = $_GET['nb'];
+$_GET['date'] = '2024-01-01';
+$_GET['nb'] = 5;
+
 $taches = [
-	["Tâche 1", "Texte descriptif ici.", 3, "en retard"],
-	["Tâche 2", "Texte descriptif ici.", 4, "en cours"],
-	["Tâche 3", "Texte descriptif ici.", 4, "terminé"],
-	["Tâche 4", "Lorem ipsum dolor sit", 4, "bloqué"],
-	["Tâche 5", "Description supplémentaire.", 2, "en attente"],
-	["Tâche 6", "Texte ici.", 5, "à valider"],
-	["Tâche 7", "Un autre texte descriptif.", 3, "prioritaire"]
+	"2024-01-01" => [
+		["Tâche 1", "Texte descriptif ici.", 3, "En retard"],
+		["Tâche 2", "Texte descriptif ici.", 4, "En cours"],
+		["Tâche 3", "Texte descriptif ici.", 4, "Terminée"]
+	],
+	"2024-01-02" => [
+		["Tâche 4", "Lorem ipsum dolor sit", 4, "Bloquée"],
+		["Tâche 5", "Description supplémentaire.", 2, "Pas commencée"]
+	],
+	"2024-01-03" => [
+		["Tâche 6", "Texte ici.", 2, "En retard"],
+		["Tâche 7", "Un autre texte descriptif.", 3, "En cours"]
+	],
+	"2024-01-04" => [
+		["Tâche 6", "Texte ici.", 1, "Bloquée"],
+		["Tâche 7", "Un autre texte descriptif.", 3, "Bloquée"]
+	],
+	"2024-01-05" => [
+		["Tâche 6", "Texte ici.", 2, "Terminée"],
+		["Tâche 7", "Un autre texte descriptif.", 3, "Terminée"]
+	]
 ];
+
 ?>
+
+<div>
+	<div class="pt-4"></div>
+	<div class="d-flex justify-content-between mx-4" id="vues">
+		<div class="left d-flex">
+			<?= tabs() ?>
+			<?= export() ?>
+			<?= filter() ?>
+		</div>
+		<div class="right">
+			<?= newTache() ?>
+		</div>
+	</div>
+</div>
+
+<div class="d-flex justify-content-center mt-4">
+	<?= CalendarRange(new DateTime(), 5) ?>
+</div>
 
 <div class="container mt-4">
-	<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-<?= $nb ?> g-4">
-		<?php foreach ($taches as $tache): ?>
-			<div class="col">
-				<?=CalendarItemTitle()?>
-				<?=generateCard($tache[0], $tache[1], $tache[2], $tache[3]);?>
+	<div class="d-flex justify-content-between">
+		<?php foreach ($taches as $dateString => $tasks): ?>
+			<div>
+				<?php 
+					$date = new DateTime($dateString);
+				?>
+				<?= CalendarItemTitle($date) ?>
+				
+				<?php foreach ($tasks as $task): ?>
+					<div class="mb-3 mt-3"> <!-- Espacement entre les cards -->
+						<?= generateCard(
+							htmlspecialchars($task[0]),
+							htmlspecialchars($task[1]),
+							htmlspecialchars($task[2]),
+							htmlspecialchars($task[3])
+						) ?>
+					</div>
+				<?php endforeach; ?>
 			</div>
 		<?php endforeach; ?>
 	</div>
