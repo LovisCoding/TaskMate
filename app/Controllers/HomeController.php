5dab2739df18f5a3ff2fe8277c5ec4b178674346
LovisCoding <arthur.lecomte@etu.univ-lehavre.fr>
2024-11-28 16:59:35 +0100

    celui qui delete je l'eteint / add preferences handling and new group component to home page

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,10 +3,12 @@
 namespace App\Controllers;
 
 use App\Models\TaskModel;
+use App\Models\PreferencesModel;
 use DateTime;
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
 
 class HomeController extends BaseController
 {
@@ -68,14 +70,12 @@ class HomeController extends BaseController
 			$defaultStates = ["inProgress", "finished"];
 		}
 		$date = $existingParams['date'] ?? $dateRange;
-		$nb = $existingParams['nb'] ?? 7;
 		$endDate = $existingParams['end_date'] ?? null;
 		$taskGroups = $existingParams['task_groups'] ?? null;
 		$priority = $existingParams['priority'] ?? null;
 		$states = $existingParams['states'] ?? $defaultStates;
 		$sort = $existingParams['sort'] ?? 'deadline';
 		$sortOrder = $existingParams['sort_order'] ?? 'asc';
-		$perPage = $this->request->getGet('perPage') ?? 5;
 		$page = $this->request->getGet('page') ?? 1;
 
 		$stateOptions = [
@@ -103,6 +103,12 @@ class HomeController extends BaseController
 		// Récupération des tâches filtrées
 		$taskModel = new TaskModel();
 		$tasks = [];
+
+		// Récupération des préférences
+		$preferencesModel = new PreferencesModel();
+		$preferences = $preferencesModel->getPreferencesByIdAccount($id_account);
+		$nb = $preferences['displayed_days_in_calendar'];
+		$perPage =  (int)$preferences['rows_per_page'];
 		
 		$tasks = match ($type) {
 			'priority' => $taskModel->getTasksByPriority($id_account, $priority, $translatedStates, $sort, $sortOrder, $perPage, $page),
@@ -130,7 +136,8 @@ class HomeController extends BaseController
 				'sort' => $sort,
 				'sortOrder' => $sortOrder
 			],
-			'queryParams' => $existingParams
+			'queryParams' => $existingParams,
+			'preferences' => $preferences,
 		]);
 		echo view('layout/footer');
 	}
