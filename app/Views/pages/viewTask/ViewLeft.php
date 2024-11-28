b93bb92078d98dbd9a1cb85325448ed4918c5ded
LovisCoding <arthur.lecomte@etu.univ-lehavre.fr>
2024-11-28 09:49:14 +0100

    update tasks

--- app/Views/pages/viewTask/ViewLeft.php
+++ app/Views/pages/viewTask/ViewLeft.php
@@ -8,22 +8,25 @@ $arr = [
 
 <div class="col-sm-12 col-lg-6 p-5 bg-white rounded-2">
 	<?= view('components/Input', ['name' => 'task_name', 'label' => null, 'type' => 'text', 'placeholder' => '', 'value' => $title]) ?>
-	<textarea name="task_desc" class="form-control mb-3 mt-3" placeholder="Description" rows="5"> <?= $description ?> </textarea>
-	<?php if ($state !== "Terminée") { ?>
-				<?= view('components/Button', ['text' => "Commencer la tâche", 'type' => null, 'name' => 'action', 'value' => 'start', 'disabled' => false] ) ?>
-			<?php } ?>
+	<textarea name="task_desc" class="form-control mb-3 mt-3" placeholder="Description"
+		rows="5"> <?= $description ?> </textarea>
+	<div class="form-check form-switch">
+		<input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" name="taskIsStarted" <?= $isChecked ? 'checked' : '' ?>>
+		<label class="form-check-label" for="flexSwitchCheckDefault">Tâche commencée</label>
+	</div>
+	<div class="py-3"></div>
 	<?= view('pages/viewTask/Commentaries', ['commentaires' => $commentaries, 'pager' => $pager]) ?>
 	<div class="d-flex justify-content-between mt-5">
 
-		
-			<?= view('components/Button', ['text' => "Supprimer", 'type' => 'danger', 'name' => 'action', 'value' => 'delete', 'disabled' => $id == -1]) ?>
+
+		<?= view('components/Button', ['text' => "Supprimer", 'type' => 'danger', 'name' => 'action', 'value' => 'delete', 'disabled' => $id == -1]) ?>
 
 
 		<?php if ($state == "En cours") { ?>
 			<?= view('components/Button', ['text' => "Terminer la tâche", 'type' => null, 'name' => 'action', 'value' => 'complete', 'disabled' => false]) ?>
 		<?php } ?>
 		<div class="d-flex justify-content-center mb-3">
-					<?= view('components/Button', ['text' => "Enregistrer les modifications", 'type' => null, 'name' => 'action', 'value' => 'save', 'disabled' => false]) ?>
-				</div>
+			<?= view('components/Button', ['text' => "Enregistrer les modifications", 'type' => null, 'name' => 'action', 'value' => 'save', 'disabled' => false]) ?>
+		</div>
 	</div>
 </div>
