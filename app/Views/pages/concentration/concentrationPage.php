394db3b53e16e49c8c91e7fefb518df3ea5b4a20
Florian Janot <janotflorian@gmail.com>
2024-11-28 15:49:57 +0100

    style fixed

--- app/Views/pages/concentration/concentrationPage.php
+++ app/Views/pages/concentration/concentrationPage.php
@@ -7,31 +7,37 @@
 
 			<div class="concentration-section-task">
 
-				<h2>
-					<?= $data['title'] ?>
-				</h2>
+				<div class="d-flex flex-column mb-1 gap-2" >
 
-				<div class="concentration-description mb-2">
-					<?= $data['description'] ?>
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil, quibusdam laboriosam amet necessitatibus fuga consequuntur consequatur explicabo dignissimos enim ab. Mollitia ullam facere assumenda animi error ut voluptatibus rem soluta.
-				</div>
+					<h2 class="d-flex align-items-center justify-content-center">
+						<?= $data['title'] ?>
+					</h2>
 
-				<div class="container text-center">
-					<div class="mb-2">
-						<span id="timerDisplay" class="fs-1">
+					<div class="container text-center">
+						<span id="timerDisplay" class="fs-1 mb-2">
 							<input type="number" id="timerInputMinutes" class="fs-1 form-control mx-auto d-inline no-spinner" min="0" max="59" value="00" />
 							:
 							<input type="number" id="timerInputSeconds" class="fs-1 form-control mx-auto d-inline no-spinner" min="0" max="59" value="00" />
 						</span>
+						<div>
+							<div id="startPauseButton" style="width:6rem;" class="btn btn-success">Démarrer</div>
+							<div id="resetTimerButton" style="width:6rem;" class="btn btn-secondary">Effacer</div>
+						</div>
+
 					</div>
-					<div>
-						<div id="startPauseButton" class="btn btn-success">Démarrer</div>
-						<div id="resetTimerButton" class="btn btn-secondary">Effacer</div>
-					</div>
+
+
 				</div>
 
+
+				<div class="concentration-description">
+					<?= $data['description'] ?>
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil, quibusdam laboriosam amet necessitatibus fuga consequuntur consequatur explicabo dignissimos enim ab. Mollitia ullam facere assumenda animi error ut voluptatibus rem soluta.
+				</div>
+
+
 			</div>
-			<div class="concentration-section-button mt-3">
+			<div class="concentration-section-buttons mt-3">
 				<button type="submit" class="btn btn-secondary">Ignorer la tâche</button>
 				<button type="submit" class="btn btn-primary">Terminer la tâche</button>
 			</div>
