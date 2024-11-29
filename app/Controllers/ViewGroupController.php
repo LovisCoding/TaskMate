2c0a68c39d724d0e9886f0b4fde759fcb0862b64
Florian Janot <janotflorian@gmail.com>
2024-11-29 09:37:18 +0100

    front newGroup

--- app/Controllers/ViewGroupController.php
+++ app/Controllers/ViewGroupController.php
@@ -4,11 +4,15 @@ namespace App\Controllers;
 
 class ViewGroupController extends BaseController
 {
-	public function index() 
-	{
-		echo view('layout/header');
-		echo view('layout/navbar');
-		echo view('pages/viewGroup');
-		echo view('layout/footer') ;
-	}
+    public function index()
+    {
+        if (!session()->get('isLoggedIn')) {
+            return redirect()->to('/');
+        }
+
+        echo view('layout/header');
+        echo view('layout/navbar');
+        echo view('pages/viewGroup');
+        echo view('layout/footer');
+    }
 }
--- app/Views/pages/viewGroup.php
+++ app/Views/pages/viewGroup.php
@@ -1,23 +1,92 @@
 
 
+<?php
 
-<h1>Donova c'est mon amoureux secret</h1>
-<h6>Et gars il est une <b>GROSSE</b> beteu </h6>
+$taskList = [
+    [
+        'id' => 1,
+        'name' => 'Faire les courses',
+        'isChecked' => false,
+    ],
+    [
+        'id' => 2,
+        'name' => 'Terminer le rapport',
+        'isChecked' => true,
+    ],
+    [
+        'id' => 3,
+        'name' => 'Appeler le client',
+        'isChecked' => false,
+    ],
+    [
+        'id' => 4,
+        'name' => 'Préparer la réunion',
+        'isChecked' => true,
+    ],
+    [
+        'id' => 5,
+        'name' => 'Nettoyer le bureau',
+        'isChecked' => false,
+    ],
+    [
+        'id' => 5,
+        'name' => 'Nettoyer le bureau',
+        'isChecked' => false,
+    ],
+    [
+        'id' => 5,
+        'name' => 'Nettoyer le bureau',
+        'isChecked' => false,
+    ],
+    [
+        'id' => 5,
+        'name' => 'Nettoyer le bureau',
+        'isChecked' => false,
+    ],
+    [
+        'id' => 5,
+        'name' => 'Nettoyer le bureau',
+        'isChecked' => false,
+    ],
+    [
+        'id' => 5,
+        'name' => 'Nettoyer le bureau',
+        'isChecked' => false,
+    ],
+    [
+        'id' => 5,
+        'name' => 'Nettoyer le bureau',
+        'isChecked' => false,
+    ],
+];
 
-
-
-<div>
-
-
-<div>
-    label
-</div>
-
-
-<div>
-    <div>search</div>
+?>
     
-</div>
 
+<div class="viewgroup-content">
+
+    <div class="viewgroup-container">
+
+        <h4>
+            Créer un nouveau groupe
+        </h4>
+
+        <div class="viewgroup-inputname" >
+        <?= view('components/Input', ['name' => 'group_name', 'label' => null, 'type' => 'text', 'placeholder' => 'Nom du groupe', 'value' => '']) ?>
+        </div>
+
+        <div class="viewgroup-list" >
+            <div class="viewgroup-listinput">
+                <label style="margin:0;padding:0;"><img src="/assets/imgs/search.svg" width="17px" height="auto" style="margin:0;padding:0;" /></label>
+                <input type="text" placeholder="Rechercher une tâche" />
+            </div>
+
+            <div class="viewgroup-listcontent" >
+                <?= view('pages/viewTask/TaskCheckBoxes', ['taskList' => $taskList ]) ?>
+            </div>
+
+        </div>
+
+    </div>
 
 </div>
