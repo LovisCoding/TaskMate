7bc05d432a606e170bf5092fad5f64a1d1f0616c
KLS <kyliannlvs2@gmail.com>
2024-11-29 08:52:29 +0100

    Changement logique bouton Task

--- app/Views/pages/viewTask/Task.php
+++ app/Views/pages/viewTask/Task.php
@@ -3,14 +3,10 @@
 <div class="container mt-3">
 	<div class="d-flex justify-content-start mb-5 button-container mt-5">
 		<?= view('components/Button', ['text' => "<strong>Supprimer</strong>", 'type' => 'danger', 'name' => 'action', 'value' => 'delete', 'disabled' => $id == -1, 'class' => 'me-2 fw-bold', 'svgName' => 'delete']) ?>
-		
-		<?php if ($state == "En cours") { ?>
-			<?= view('components/Button', ['text' => "<strong>Terminer la tâche</strong>", 'type' => null, 'name' => 'action', 'value' => 'complete', 'disabled' => $state == 'Terminée', 'svgName' => 'check']) ?>
-		<?php } else { ?>
-			<?= view('components/Button', ['text' => "<strong>Enregistrer les modifications</strong>", 'type' => null, 'name' => 'action', 'value' => 'save', 'disabled' => $state == 'Terminée', 'svgName' => 'download']) ?>
-		<?php } ?>
+		<?= view('components/Button', ['text' => "<strong>Terminer la tâche</strong>", 'type' => null, 'name' => 'action', 'value' => 'complete', 'disabled' => $state == 'Terminée', 'svgName' => 'check']) ?>
+		<?= view('components/Button', ['text' => "<strong>Enregistrer les modifications</strong>", 'type' => null, 'name' => 'action', 'value' => 'save', 'disabled' => $state == 'Terminée', 'svgName' => 'download']) ?>
 	</div>
-	<div class="row" style="width: 100%;">
+	<div class="row w-100">
 		<?= view('components/Input', ['name' => 'task_state', 'type' => 'hidden', 'value' => $state]) ?>
 		<?= view('pages/viewTask/ViewLeft', ['title'=>$title, 'commentaries'=>$commentaries, 'isChecked' => $started] ) ?>
 		<div class="col-sm-12 col-lg-2"></div>
--- app/Views/pages/viewTask/ViewLeft.php
+++ app/Views/pages/viewTask/ViewLeft.php
@@ -1,11 +1,3 @@
-<?php
-$arr = [
-	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rhoncus, purus at fermentum pulvinar, dolor nibh auctor odio, vel consequat purus justo et dolor. Curabitur facilisis tortor velit, sit amet tempus augue mollis non. Suspendisse sollicitudin.',
-	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rhoncus, purus at fermentum pulvinar, dolor nibh auctor odio, vel consequat purus justo et dolor. Curabitur facilisis tortor velit, sit amet tempus augue mollis non. Suspendisse sollicitudin.',
-	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rhoncus, purus at fermentum pulvinar, dolor nibh auctor odio, vel consequat purus justo et dolor. Curabitur facilisis tortor velit, sit amet tempus augue mollis non. Suspendisse sollicitudin.'
-];
-?>
-
 <div class="col-sm-12 col-lg-6 p-5 bg-white rounded-2">
     <?= view('components/Input', ['name' => 'task_name', 'label' => null, 'type' => 'text', 'placeholder' => '', 'value' => $title]) ?>
     <textarea name="task_desc" class="form-control mb-3 mt-3" placeholder="Description"
